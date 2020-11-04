d9335c01f8dea5526a9d7264b41801fdf7d9030d
ratrap44 <62080086+ratrap44@users.noreply.github.com>
2020-11-04 13:40:23 -0500

    vrassignment3

---
+++ VR_Setup_Proj/Assets/Scripts/DirectButton.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.Events;
+
+public class DirectButton : XRBaseInteractable
+{
+    public UnityEvent OnPress = null;
+
+    private float ymin = 0.0f;
+    private float ymax = 0.0f;
+    private bool previousPress = false;
+
+
+    private float previousHandHeight = 0.0f;
+    private XRBaseInteractor hover = null;
+
+    protected override void Awake ()
+    {
+        base.Awake();
+        onHoverEnter.AddListener(StartPress);
+        onHoverExit.AddListener(EndPress);
+    }
+
+    private void OnDestroy()
+    {
+        onHoverEnter.RemoveListener(StartPress);
+        onHoverExit.RemoveListener(EndPress);
+
+    }
+
+    private void StartPress(XRBaseInteractor interactor)
+    {
+        hover = interactor;
+        previousHandHeight = GetLocalYPosition(hover.transform.position);
+    }
+
+    private void EndPress(XRBaseInteractor interactor)
+    {
+        hover = null;
+        previousHandHeight = 0.0f;
+
+        previousPress = false;
+        SetYPosition(ymax);
+    }
+
+    private void Start()
+    {
+        SetMinMax();
+    }
+
+    private void SetMinMax()
+    {
+        Collider collider = GetComponent<Collider>();
+        ymin = transform.localPosition.y - (collider.bounds.size.y * 0.5f);
+        ymax = transform.localPosition.y;
+    }
+
+    public override void ProcessInteractable(XRInteractionUpdateOrder.UpdatePhase updatePhase)
+    {
+        if(hover)
+        {
+            float newHandHeight = GetLocalYPosition(hover.transform.position);
+            float handDifference = previousHandHeight - newHandHeight;
+            previousHandHeight = newHandHeight;
+
+            float newPosition = transform.localPosition.y - handDifference;
+            SetYPosition(newPosition);
+
+            CheckPress();
+        }
+    }
+
+    private float GetLocalYPosition(Vector3 position)
+    {
+        Vector3 localPosition = transform.root.InverseTransformPoint(position);
+        return localPosition.y;
+    }
+
+    private void SetYPosition(float position)
+    {
+        Vector3 newPosition = transform.localPosition;
+        newPosition.y = Mathf.Clamp(position, ymin, ymax);
+        transform.localPosition = newPosition;
+    }
+
+    private void CheckPress()
+    {
+        bool inPosition = Inposition();
+
+        if (inPosition && inPosition != previousPress)
+            OnPress.Invoke();
+
+        previousPress = inPosition;
+    }
+
+    private bool Inposition()
+    {
+        float inRange = Mathf.Clamp(transform.localPosition.y, ymin, ymin + 0.01f);
+
+        return transform.localPosition.y == inRange;
+    }
+}
---
+++ VR_Setup_Proj/Assets/Scripts/Doors.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Doors : MonoBehaviour
+{
+    public GameObject door;
+    public GameObject door2;
+
+
+    public void open()
+    {
+        door.SetActive(false);
+    }
+
+    public void close()
+    {
+        door.SetActive(true);
+    }
+
+    public void opendoor2()
+    {
+        door2.SetActive(false);
+    }
+
+    public void closedoor2()
+    {
+        door2.SetActive(true);
+    }
+}
