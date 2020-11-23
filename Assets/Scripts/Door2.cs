b722760a76f81fea79560a76fb4e490e10e8c4da
ratrap44 <62080086+ratrap44@users.noreply.github.com>
2020-11-23 17:14:38 -0500

    yes

---
+++ Assets/Scripts/Door2.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door2 : MonoBehaviour
+{
+    public GameObject door;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Key2")
+        {
+            door.SetActive(false);
+        }
+    }
+
+}
