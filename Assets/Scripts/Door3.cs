4e34299886602349c6b5207ac0acd2177c30871a
ratrap44 <62080086+ratrap44@users.noreply.github.com>
2020-12-01 16:06:44 -0500

    final

---
+++ Assets/Scripts/Door3.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door3 : MonoBehaviour
+{
+    public GameObject door;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Key3")
+        {
+            door.SetActive(true);
+        }
+    }
+}
